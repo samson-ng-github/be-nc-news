ee3e1d9b7535a3a81d2e1130373d70ec14376fa2
Samson Ng <samsonng@Samsons-iMac.local>
2024-06-17 10:34:21 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const apiRouter = require('./routes/api-router');
+const cors = require('cors');
 const {
   handle400,
   handle404,
@@ -7,6 +8,7 @@ const {
 } = require('./error-handlers/error-handlers');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRouter);
 app.get('*', (req, res, next) => {
